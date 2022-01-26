7722322fe7bb7aadf381fd03d33acc1d1149f1e4
Nur Aziz <naziz0404@gmail.com>
2022-01-26 21:02:20 +0700

    update

---
+++ arduino/arduino.ino
@@ -0,0 +1,102 @@
+// Library: WebSockets
+// by: Marcus Sattler
+#include <ESP8266WiFi.h>
+#include <WebSocketsClient.h>
+#include <Hash.h>
+
+WebSocketsClient webSocket;
+
+int state = 0;
+
+#define id 1304
+#define SSID "Az"
+#define PASS ""
+#define server "192.168.43.126" 
+#define port 8010
+
+void handler(String cmd)
+{
+	if (cmd == "jemur")
+	{
+		state = 1;
+	}
+	else if (cmd == "teduh")
+	{
+		state = 0;
+	}
+}
+void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
+{
+
+	switch (type)
+	{
+	case WStype_DISCONNECTED:
+		Serial.printf("[WSc] Disconnected!\n");
+		break;
+	case WStype_CONNECTED:
+	{
+		Serial.printf("[WSc] Connected to url: %s\n", payload);
+		// send message to server when Connected
+		webSocket.sendTXT(("server "+String(id)+" set").c_str());
+	}
+	break;
+	case WStype_TEXT:
+		// Serial.printf("[WSc] get text: %s\n", payload);
+		// sx = "Dari arduino: ";
+
+		// webSocket.sendTXT(sx.c_str());
+		handler((char *)payload);
+
+		// send message to server
+		// webSocket.sendTXT("message here");
+		break;
+		// case WStype_PING:
+		// 	// pong will be send automatically
+		// 	Serial.printf("[WSc] get ping\n");
+		// 	break;
+		// case WStype_PONG:
+		// 	// answer to a ping we send
+		// 	Serial.printf("[WSc] get pong\n");
+		// 	break;
+	}
+}
+
+void setup()
+{
+	// Serial.begin(921600);
+	Serial.begin(115200);
+	delay(10);
+	//Serial.setDebugOutput(true);
+	Serial.println();
+
+	WiFi.begin(SSID, PASS);
+	Serial.print("Connecting...");
+	while (WiFi.status() != WL_CONNECTED)
+	{
+		delay(100);
+		Serial.print(".");
+	}
+	Serial.println("\nConnected");
+	Serial.print("IP: ");
+	Serial.println(WiFi.localIP());
+
+	// server address, port and URL
+	webSocket.begin(server, port, "/");
+
+	// event handler
+	webSocket.onEvent(webSocketEvent);
+
+	// try ever 5000 again if connection has failed
+	webSocket.setReconnectInterval(5000);
+	webSocket.enableHeartbeat(15000, 3000, 2);
+
+	pinMode(LED_BUILTIN, OUTPUT);
+	digitalWrite(LED_BUILTIN, 1);
+}
+
+void loop()
+{
+	digitalWrite(LED_BUILTIN, !state);
+	webSocket.loop();
+	webSocket.sendTXT(("server "+String(id)+" send "+ (state ? "DIJEMUR" : "NGEYUP")).c_str());
+}
---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const path = require('path');
+const { createServer } = require('http');
+
+const WebSocket = require('ws');
+
+const app = express();
+app.use(express.static(path.join(__dirname, '/templates')));
+
+const server = createServer(app);
+const wss = new WebSocket.Server({ server });
+
+var servers = {}
+var clients = {}
+
+wss.on("connection", function (ws) {
+  let _id = ""
+  let type = ""
+  ws.on('close', function () {
+    if (type == "server") {
+      console.log('Server Disconected: '+_id);
+      delete servers[_id]
+    }
+    else {
+      console.log("Client disconected: "+_id)
+      delete clients[_id]
+    }
+  });
+  ws.on("message", (data, bin) => {
+    let dat = data.toString()
+    let args = dat.split(" ")
+    let side = args[0]
+    let id = args[1]
+    let command = args[2]
+    let msg = args.slice(3)?.join(" ")
+    _id = id
+    type = side
+
+    let serverList = Object.keys(servers)
+    let clientList = Object.keys(clients)
+
+    switch (command) {
+      case "set":
+        if (side == "server") {
+          console.log("sever connected: " + id)
+          servers[id] = ws
+        }
+        else {
+          console.log("client connected: " + id)
+          clients[id] = ws
+        }
+        break;
+      case "send":
+        if (side == "server") {
+          if (clientList.includes(id)) clients[id].send(msg)
+          // else console.log([clientList, id])
+        }
+        else {
+          if (serverList.includes(id)) servers[id].send(msg)
+          // else console.log([serverList, id])
+        }
+        break;
+    }
+
+
+  })
+});
+
+server.listen(8010, function () {
+  console.log('Listening on http://localhost:8010');
+});
